7a3f1f0b67ee6fcb0da96314ad388cb7bf400277
Rockey Biswas <85108386+ncrptd@users.noreply.github.com>
2023-09-18 15:03:21 +0530

    ref:

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Header from './components/Header'
-import Contacts from './components/contacts'
+import Contacts from './components/Contacts'
 import { useContacts } from './hooks/ContactHooks'
 
 function App() {
